54ab4d8a6032dbfa3f1669d551f1558e9c43ca37
AnasAhmedFND <anasahmed4402@gmail.com>
2025-12-20 13:37:54 +0600

    footer text bold

--- src/Compunents/Footer.jsx
+++ src/Compunents/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
    <section>
-    <div className="border py-20 bg-amber-500">
+    <div className="border py-20 bg-amber-500 text-2xl text-center font-bold ">
         Footer section
     </div>
    </section>

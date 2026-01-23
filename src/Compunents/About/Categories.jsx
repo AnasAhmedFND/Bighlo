d132f0e6b0ce7934a26b4516bfeaf56e148068be
AnasAhmedFND <anasahmed4402@gmail.com>
2026-01-23 23:27:11 +0600

    add category by filter api products

--- src/Compunents/About/Categories.jsx
+++ src/Compunents/About/Categories.jsx
@@ -1,38 +1,73 @@
+"use client"
+import React, { useEffect, useState } from 'react'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
+const Categories =  () => {
 
-import React from 'react'
+  const [products, setProducts]=useState([]);
 
-const Categories = async () => {
+   useEffect(() => {
+    const loadProducts = async () => {
+      const res = await fetch("https://dummyjson.com/products");
+      const data = await res.json();
 
-  const data = await fetch('https://dummyjson.com/products?&limit=0')
-  const  producted = await data.json()
-  
-  const filtered = producted.products.filter(
-  (item) => item.category === "smartphones"
-);
+      const filtered = data.products.filter((item) => item.category === "beauty")
+      setProducts(filtered);
+    };
 
+    loadProducts();
+  }, []);
+ 
+  // const find =data.products.filter((item) => item.id <= 10 )
+  // console.log(find)
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 2000,
+    autoplaySpeed: 2000,
+    cssEase: "linear"
+  };
   
   return (
     <section className='container  mx-auto border py-10'>
         <h3 className='font-bold text-2xl '>Shopping by Categories </h3>
         {/* main_div_items's */}
-        <div className="flex flex-wrap justify-between gap-4  mt-4 ">
-          {filtered.map((item, index) =>(
-            <div className="border p-2 rounded-t-xl w-[23%] ">
-             <img src={item.thumbnail} alt={item.title} />
-             <div className="flex justify-between px-4 py-2 ">
-              <p>ID: {item.id} </p>
-              <h2 className='font-bold'>{item.title} </h2>
+        <div className="  mt-4 ">
 
-             </div>
-              <div className="border p-2 rounded-2xl flex justify-between items-center ">
-                <p className='flex items-center'><span className='font-bold text-red-500 text-2xl '>$ </span> - {item.price} </p>
-                <p ><span className='font-bold text-xl text-red-500 line-through'> 50%Off </span> <small> (For-Rifa) </small> </p>
-              </div>
+          <div className="slider-container  ">
+            <Slider {...settings}>
+        
+              {products.map((item, index) =>(
+                <div className="flex px-4 justify-between ">
+                  <div  className="border p-2 pb-4  rounded-t-xl w-[80%] bg-[#F5F5F5]  ">
+                  <img src={item.thumbnail} alt={item.title} />
+                  <div className="flex justify-between px-4 py-2 border-t-2 ">
+                    <p>ID: {item.id} </p>
+                    <h2 className='font-bold'>{item.title} </h2>
 
-            </div>
-            
-          ))}
+                  </div>
+                    <div className="border p-2 rounded-2xl flex justify-between items-center ">
+                      <p className='flex items-center'><span className='font-bold text-red-500 text-2xl '>$ </span> - {item.price} </p>
+                      <p ><span className='font-bold text-xl text-red-500 line-through'> 50%Off </span> <small> (For-Rifa) </small> </p>
+                    </div>
+
+                  </div>
+
+                </div>
+
+              
+            ))}                                 
+          
+          
+              </Slider>
+          </div>
+
 
         </div>
 

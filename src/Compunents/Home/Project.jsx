880c8b7f9a05b83e7487b27cced4f7aa8ba7ec8f
AnasAhmedFND <anasahmed4402@gmail.com>
2026-06-01 12:39:28 +0300

    project and team section responsive done in page home

--- src/Compunents/Home/Project.jsx
+++ src/Compunents/Home/Project.jsx
@@ -6,18 +6,18 @@ const jonst = Jost({
 })
 const Project = () => {
     return (
-        <div className='bg-[#F5F5F5]'>
+        <div className='bg-[#F5F5F5] md:px-0 px-2   '>
         <section className='container mx-auto text-center  py-10 '>
             <p className='text-blue-400 '>CASE STUDIES</p>
             <h3 className={`${jonst.className} font-bold mt-2 text-3xl `}>Our successful project</h3>
             {/* cards */}
-            <div className="flex gap-5 mt-10 justify-center">
+            <div className="md:flex md:gap-5 mt-10 justify-center">
                 {/* one */}
-                <article className=' w-[25%] relative '>
+                <article className=' md:w-[25%]   relative '>
                     <img className='' src="/home/pro1 (1).png" alt="project" />
 
                     <div className="h-[200px] px-2 shadow-2xl rounded-b-lg ">
-                        <img className='absolute top-[315px] left-1/2 -translate-1/2 ' src="/home/pro1 (4).png" alt="" />
+                        <img className='absolute md:top-[315px] top-[355px] left-1/2 -translate-1/2 ' src="/home/pro1 (4).png" alt="" />
                         <h6 className='pt-10 font-bold'>Artificial Intelligence</h6>
                         <p className='mt-2'>Lorem ipsum amet, consectetur adipisicing elit,
                             sed do eiusmod tempor incididunt labore et
@@ -30,11 +30,11 @@ const Project = () => {
                 </article>
 
                  {/* two */}
-                <article className=' w-[25%] relative'>
+                <article className=' md:w-[25%] md:mt-0 mt-6  relative'>
                     <img src="/home/pro1 (2).png" alt="project" />
 
                      <div className="h-[200px] px-2 shadow-2xl rounded-b-lg ">
-                        <img className='absolute top-[315px] left-1/2 -translate-1/2 ' src="/home/pro1 (4).png" alt="" />
+                        <img className='absolute md:top-[315px] top-[355px] left-1/2 -translate-1/2 ' src="/home/pro1 (4).png" alt="" />
                         <h6 className='pt-10 font-bold'>Rebot Technology View</h6>
                         <p className='mt-2'>Lorem ipsum amet, consectetur adipisicing elit,
                             sed do eiusmod tempor incididunt labore et
@@ -47,11 +47,11 @@ const Project = () => {
                 </article>
 
                  {/* three */}
-                <article className=' w-[25%] relative'>
+                <article className=' md:w-[25%] md:mt-0 mt-6  relative'>
                     <img src="/home/pro1 (3).png" alt="project" />
 
                     <div className="h-[200px] px-2 shadow-2xl rounded-b-lg ">
-                        <img className='absolute top-[315px] left-1/2 -translate-1/2 ' src="/home/pro1 (4).png" alt="" />
+                        <img className='absolute md:top-[315px] top-[355px] left-1/2 -translate-1/2 ' src="/home/pro1 (4).png" alt="" />
                         <h6 className='pt-10 font-bold'>Data Scientest Code</h6>
                         <p className='mt-2'>Lorem ipsum amet, consectetur adipisicing elit,
                             sed do eiusmod tempor incididunt labore et
--- src/Compunents/Home/Team.jsx
+++ src/Compunents/Home/Team.jsx
@@ -3,34 +3,34 @@ import React from 'react'
 const Team = () => {
   return (
     <>
-    <section className=' container mx-auto  mt-10 py-10'>        
+    <section className=' container mx-auto  md:mt-10 py-10 md:px-0 px-2 '>        
       <p className='text-red-500  '><small> TEAM MEMBERS </small></p>
       <h3 className='text-2xl font-bold '>Our best team person</h3>
       {/* mother div team */}
-      <div className=' flex justify-between mt-5'>
+      <div className=' md:flex md:justify-between mt-5'>
         {/* team-1 */}
-        <div className=''>
-          <img src="/home/teme1 (1).png" alt="man" />
+        <div className=' '>
+          <img className='w-full md:w-auto' src="/home/teme1 (1).png" alt="man" />
           <p className='font-bold '>Cameron Williamson</p>
           <p><small>Founder & CEO </small> </p>
         </div>
         {/* two */}
-        <div className="">
-          <img src="/home/teme1 (2).png" alt="man" />
+        <div className="mt-4 ">
+          <img className='w-full md:w-auto' src="/home/teme1 (2).png" alt="man" />
           <p className='font-bold '>Jacob Jones</p>
           <p><small>Product Manager </small> </p>
 
         </div>
         {/* three */}
-        <div className="">
-          <img src="/home/teme1 (3).png" alt="man" />
+        <div className="mt-4 ">
+          <img className='w-full md:w-auto' src="/home/teme1 (3).png" alt="man" />
           <p className='font-bold '>Esther Howards</p>
           <p><small>Marketing Head </small> </p>
 
         </div>
         {/* four */}
-        <div className="">
-          <img src="/home/teme1 (4).png" alt="man" />
+        <div className="mt-4 ">
+          <img className='w-full md:w-auto' src="/home/teme1 (4).png" alt="man" />
           <p className='font-bold '>Robert Fox</p>
           <p><small>Team Head </small> </p>
 

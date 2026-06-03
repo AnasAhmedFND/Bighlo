729a79b3dd2a85c00522ada196c521b443d02772
AnasAhmedFND <anasahmed4402@gmail.com>
2026-06-03 07:04:14 +0300

    responsive done payment method

--- src/Compunents/Payment/Payment.jsx
+++ src/Compunents/Payment/Payment.jsx
@@ -16,39 +16,39 @@ const kablammo = Kablammo({
 
 const Payment = () => {
   return (
-    <section className='container mx-auto   py-10 px-5   '>
+    <section className='container mx-auto   md:py-10 md:px-5    '>
 
-      <div className="shadow-2xl py-10 px-5 flex">
+      <div className="shadow-2xl py-10 md:px-5 px-2 md:flex">
       {/* left div.....................//////////////////////////// */}
-      <div className="border-r-2 w-[60%] ">
+      <div className="md:border-r-2 md:w-[60%] ">
         <h2 className='font-bold text-2xl flex items-center gap-4' > <Link href={'/cart'} > <FaArrowLeft /></Link>  Checkout</h2>
 
         {/* 1. Contact Information....................//////////////////// */}
         <div className="mt-8">
           <h5 className='font-bold '>1.Contact Information</h5>
 
-          <div className="flex gap-5 mt-5">
+          <div className="md:flex gap-5 mt-5">
             <div className="">
               <p className='text-[#7e8c8cec] '>FIRST NAME</p>
               <input className='border rounded-lg px-2' type="text" placeholder='First name..' />
             </div>
-            <div className="">
+            <div className="md:mt-0 mt-2">
               <p className='text-[#7e8c8cec] ' >LAST NAME</p>
-              <input className='border rounded-lg px-2 ' type="text" placeholder='Last name..' />
+              <input className='border rounded-lg px-2 md:w-auto w-full ' type="text" placeholder='Last name..' />
             </div>
 
           </div>
           {/* Phone & E-mail .......................................*/}
-          <div className="flex mt-5 gap-5">
+          <div className="md:flex mt-5 gap-5">
             <div className="">
               <p className='text-[#7e8c8cec] '>PHONE</p>
               <input className='border rounded-lg mt-2 px-2 ' type="number" placeholder='xxxxx...xxxx' />
 
             </div>
 
-            <div className="">
+            <div className="md:mt-0 mt-2">
               <p className='text-[#7e8c8cec] '>E-MAIL</p>
-              <input className='border rounded-lg mt-2 px-2 ' type="email" placeholder='Enter your e-mail..' />
+              <input className='border rounded-lg mt-2 px-2 md:w-auto w-full ' type="email" placeholder='Enter your e-mail..' />
             </div>
 
           </div>
@@ -65,31 +65,31 @@ const Payment = () => {
 
           </div>
 
-          <div className="flex mt-5 gap-5">
+          <div className="md:flex mt-5 gap-5">
             <div className="">
               <p className='text-[#7e8c8cec] '>DELIVERY DATE</p>
               <input className='mt-2 border rounded-lg px-2 w-[250px]  ' type="date" />
             </div>
-            <div className="">
+            <div className="md:mt-0 mt-2">
               <p className='text-[#7e8c8cec] '>CONVENIENT TIME</p>
               <input className='mt-2 border rounded-lg px-2 w-[250px]  ' type="time" />
             </div>
           </div>
           {/* city & address.................................... */}
-          <div className="flex mt-8 gap-4">
+          <div className="md:flex mt-8 gap-4">
             <div className="">
               <p className='text-[#7e8c8cec] '>CITY</p>
-              <input className='border mt-2 rounded-lg w-[200px] ' type="city" />
+              <input className='border mt-2 rounded-lg md:w-[200px] w-full ' type="city" />
             </div>
 
             <div className="">
-              <p className='text-[#7e8c8cec] '>ADDRESS</p>
-              <input className='border mt-2 rounded-lg w-[200px] ' type="address" />
+              <p className='text-[#7e8c8cec] md:mt-0 mt-2'>ADDRESS</p>
+              <input className='border mt-2 rounded-lg md:w-[200px] w-full ' type="address" />
             </div>
 
             <div className="">
-              <p className='text-[#7e8c8cec] '>ZIP CODE</p>
-              <input className='border mt-2 rounded-lg w-[200px] ' type="number" />
+              <p className='text-[#7e8c8cec] md:mt-0 mt-2'>ZIP CODE</p>
+              <input className='border mt-2 rounded-lg md:w-[200px] w-full ' type="number" />
             </div>
 
 
@@ -100,7 +100,7 @@ const Payment = () => {
         {/* Payment method .........................//////////////////...*/}
         <h4 className='font-bold mt-8'>3.Payment method</h4>
 
-        <div className="flex gap-5">
+        <div className="grid grid-flow-row md:grid-cols-5 grid-cols-3 gap-5">
           <img className=' border rounded-lg w-[120px] h-[60px]  py-2 px-4 hover:border-2 cursor-pointer ' src="/Payment/mastercard.png" alt="payment_logo" />
           <img className=' border rounded-lg w-[120px] h-[60px]  py-2 px-4 hover:border-2 cursor-pointer ' src="/Payment/visa.png" alt="payment_logo" />
           <img className=' border rounded-lg w-[120px] h-[60px]  py-2 px-4 hover:border-2 cursor-pointer ' src="/Payment/bkash.png" alt="payment_logo" />
@@ -112,10 +112,10 @@ const Payment = () => {
       </div>
 
       {/* Right div,...................................//////////// */}
-      <div className="w-[40%] pl-2  ">
+      <div className="md:w-[40%] pl-2   ">
         <div className="flex relative border-b ">
         <img className=' w-full ' src="/cart/model.png" alt="person" />
-        <h4 className={`${kablammo.className} font-bold text-4xl absolute bottom-[50px] left-0`} >WELCOME to <br />  <span className={`${caveat.className} `} >RIFANAS </span>  <br /> FASHONE </h4>
+        <h4 className={`${kablammo.className} font-bold text-4xl absolute bottom-[50px] left-0`} >WELCOME to <br />  <span className={`${caveat.className} `} >RIFANAS </span>  <br /> Payment </h4>
         
         </div>
         <img src="/payment/Heading 2.png" alt="" />
